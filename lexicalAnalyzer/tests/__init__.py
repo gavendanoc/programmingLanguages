b8d21dafaa070bf38e388744fb5a4f3ee4d6d8bd
gavendanoc <gabriel19oct@hotmail.com>
2020-09-24 17:33:42 -0500

    :bug: solved import bug

--- lexicalAnalyzer/tests/__init__.py
+++ lexicalAnalyzer/tests/__init__.py
@@ -1,11 +1,11 @@
-from lexical import practica1
+from lexical import main
 
 def getLexicalOutput(code):
   data = [f"{line}\n" for line in code.split("\n")]
   out = []
   try:
-    for token in practica1.getTokens(data):
+    for token in main.getTokens(data):
       out.append(str(token))
-  except practica1.LexicalError as le:
+  except main.LexicalError as le:
     out.append(le.message)
   return '\n'.join(out)
